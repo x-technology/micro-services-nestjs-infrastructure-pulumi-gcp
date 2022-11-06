9cda9dc9993e7b6a334c279e5aebc704def1c750
Andrew Red <andrew@reddikh.com>
2022-11-06 14:34:43 +0000

    - fixed infra

--- infra/index.ts
+++ infra/index.ts
@@ -1,9 +1,10 @@
-import { kubeconfig } from "./cluster";
+import { kubeconfig, cluster } from "./cluster";
 import { ingressServiceIP } from "./k8s/system";
-import { currencyConverter } from "./k8s/apps";
 import { mainRecord } from "./dns";
+import { currencyConverter } from "./k8s/apps";
 
+export const clusterName = cluster.name;
 export const config = kubeconfig;
 export const externalIp = ingressServiceIP;
-export const domain = mainRecord;
-export const service = currencyConverter;
+export const domain = mainRecord.name;
+export const service = currencyConverter.urn;
